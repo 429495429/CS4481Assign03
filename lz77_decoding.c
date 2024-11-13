d3a78616479ba42ebdc30c35aafd492d11d0183c
Zijian Yang <53712248+429495429@users.noreply.github.com>
2024-11-13 16:25:29 +0000

    update

--- lz77_decoding.c
+++ lz77_decoding.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "pnm_library_01_2022/libpnm.h"
 #include "lz77_decoding_function.h"
 
@@ -9,6 +10,13 @@ int main(int argc, char *argv[]){
         printf("Please input in format: %s <filename> \n", argv[0]);
         return 1;
     }
+    clock_t start, end;
+    double cpu_time_used;
+
     char *filename = argv[1];
+    start = clock();  // time record start
     Decode_Using_LZ77(filename);
+    end = clock();// time record end
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;  
+    printf("decoding time %f seconds\n", cpu_time_used);
 }
--- lz77_encoding.c
+++ lz77_encoding.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "pnm_library_01_2022/libpnm.h"
 #include "lz77_encoding_function.h"
 
@@ -9,10 +10,18 @@ int main(int argc, char *argv[]){
         printf("Please input in format: %s <imagename> <filename>\n", argv[0]);
         return 1;
     }
+    clock_t start, end;
+    double cpu_time_used;
+
     char *filename = argv[1];
     unsigned int buffersize = atoi(argv[2]);
     float avg_offset, std_offset, avg_length, std_length;
+
+    start = clock();  // time record start
     Encode_Using_LZ77(filename,buffersize,&avg_offset,&std_offset,&avg_length,&std_length);
+    end = clock();// time record end
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;  
+    printf("encoding time %f seconds\n", cpu_time_used);
 
     printf("offset avg %f std %f \n", avg_offset, std_offset);
     printf("length avg %f std %f \n", avg_length, std_length);
